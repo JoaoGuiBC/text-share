1ddb3ee395933a43bfd0f83fff03522b4e97f9ba
JoaoGuiBC <joaoguibc@gmail.com>
2021-10-20 21:47:27 -0300

    Created a authentication context that have the ability to make login using github

---
+++ web/src/contexts/auth.tsx
@@ -0,0 +1,76 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+type User = {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
+};
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+type AuthResponse = {
+  token: string;
+  user: User;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${
+    import.meta.env.VITE_GITHUB_CLIENT_ID
+  }`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    });
+
+    const { token, user: responseUser } = response.data;
+
+    localStorage.setItem('@textshare:token', token);
+
+    setUser(responseUser);
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('@textshare:token');
+
+    if (token) {
+      api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+      api.get<User>('profile').then((response) => {
+        setUser(response.data);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=');
+
+      window.history.pushState({}, '', urlWithoutCode);
+
+      signIn(githubCode);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signInUrl, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}

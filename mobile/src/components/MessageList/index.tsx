bff28ae4fce1f0106e5688eebc4876cfdd48eb6b
JoaoGuiBC <joaoguibc@gmail.com>
2021-10-24 17:34:38 -0300

    Receveing new messages through socket.io

--- mobile/src/components/MessageList/index.tsx
+++ mobile/src/components/MessageList/index.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
+import io from 'socket.io-client';
 
 import { api } from '../../services/api';
 import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
+
+const messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+
+socket.on('new_message', (newMessage) => {
+  messagesQueue.push(newMessage);
+});
 
 export function MessageList() {
   const [currentMessages, setCurrentMessage] = useState<MessageProps[]>([]);
@@ -18,6 +27,20 @@ export function MessageList() {
     }
 
     getMessages();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessage((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+
+        messagesQueue.shift();
+      }
+    }, 2000);
+
+    return () => clearInterval(timer);
   }, []);
 
   return (

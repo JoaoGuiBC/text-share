284c7b37a4518fb4b5cf1ca61f7c7bb31463b44f
JoaoGuiBC <joaoguibc@gmail.com>
2021-10-24 16:21:41 -0300

    Getting messages from server

--- mobile/src/components/MessageList/index.tsx
+++ mobile/src/components/MessageList/index.tsx
@@ -1,41 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
 
-import { Message } from '../Message';
+import { api } from '../../services/api';
+import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
 
 export function MessageList() {
+  const [currentMessages, setCurrentMessage] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function getMessages() {
+      const messagesResponse = await api.get<MessageProps[]>(
+        'messages/lastTriad'
+      );
+      setCurrentMessage(messagesResponse.data);
+    }
+
+    getMessages();
+  }, []);
+
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message
-        id="1"
-        text="Mensagem usada para testar"
-        user={{
-          name: 'John Doe',
-          avatar_url: 'https://randomuser.me/api/portraits/men/32.jpg',
-        }}
-      />
-      <Message
-        id="1"
-        text="Mensagem usada para testar"
-        user={{
-          name: 'John Doe',
-          avatar_url: 'https://randomuser.me/api/portraits/men/32.jpg',
-        }}
-      />
-      <Message
-        id="1"
-        text="Mensagem usada para testar"
-        user={{
-          name: 'John Doe',
-          avatar_url: 'https://randomuser.me/api/portraits/men/32.jpg',
-        }}
-      />
+      {currentMessages.map((message) => (
+        <Message
+          key={message.id}
+          id={message.id}
+          text={message.text}
+          user={{
+            name: message.user.name,
+            avatar_url: message.user.avatar_url,
+          }}
+        />
+      ))}
     </ScrollView>
   );
 }

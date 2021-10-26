e747b47915d593db46e993490e84d5a452971079
JoaoGuiBC <joaoguibc@gmail.com>
2021-10-25 22:23:01 -0300

    Counting occurrence of every word

---
+++ tag_cloud/lib/tag_cloud/tags/count.ex
@@ -0,0 +1,21 @@
+defmodule TagCloud.Tags.Count do
+  import Ecto.Query
+
+  alias TagCloud.Messages.Get
+
+  def call do
+    Get.today_messages()
+    |> Task.async_stream(fn message -> count_words(message.message) end)
+    |> Enum.reduce(%{}, fn element, acc -> sum_values(elem, acc) end)
+  end
+
+  defp count_words(message) do
+    message
+    |> String.split()
+    |> Enum.frequencies()
+  end
+
+  defp sum_values({:ok, map1}, map2) do
+    Map.merge(map1, map1, fn _key, value1, value2, -> value1 + value2 end)
+  end
+end
